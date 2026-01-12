777440863457c50774a5ac873487ef88cff7016f
sai <bisnunayak07@gmail.com>
2026-01-12 18:45:48 +0530

    rock, paper or scissors.game on

--- project1/script.js
+++ project1/script.js
@@ -1,2 +1,19 @@
 alert("Done")
 console.log("Hello Sk")
+let x;
+function getComputerChoice(ran){
+x= Math.floor( Math.random()*ran);
+
+if(x<=2){
+    return "rock";
+}
+else if(x<=3){
+    return "paper";
+}
+else{
+    return "scissors";
+}
+// return x;
+}
+console.log(getComputerChoice(5));
+
