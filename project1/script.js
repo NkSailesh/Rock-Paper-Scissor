133f7c8a2fad40a3039c0ebb78ec4c62ec924677
sai <bisnunayak07@gmail.com>
2026-01-15 18:44:06 +0530

    Human choice

--- project1/script.js
+++ project1/script.js
@@ -1,19 +1,32 @@
 alert("Done")
 console.log("Hello Sk")
 let x;
-function getComputerChoice(ran){
-x= Math.floor( Math.random()*ran);
+// function getComputerChoice(ran){
+// x= Math.floor( Math.random()*ran);
 
-if(x<=2){
-    return "rock";
+// if(x<=2){
+//     return "rock";
+// }
+// else if(x<=3){
+//     return "paper";
+// }
+// else{
+//     return "scissors";
+// }
+// // return x;
+// }
+// console.log(getComputerChoice(5));
+let y;
+function getHumanChoice(y){
+    if(y==1){
+        console.log("Rock")
+    }
+     else if(y==2){
+        console.log("Paper")
+    }
+     else{
+        console.log("Scissors")
+    }
 }
-else if(x<=3){
-    return "paper";
-}
-else{
-    return "scissors";
-}
-// return x;
-}
-console.log(getComputerChoice(5));
-
+let Human_Choice = prompt("Enter your Number-1/2/3");
+getHumanChoice(Human_Choice);
