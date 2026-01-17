455ae684a400ec13af1c02c4029594e3b9bda17e
sai <bisnunayak07@gmail.com>
2026-01-17 19:28:35 +0530

    User choice

--- project1/script.js
+++ project1/script.js
@@ -3,8 +3,19 @@ console.log("Hello Sk")
 function getComputerGeneretion(){
     const random_string=["rock","paper","scissor"];
 const randomchoices=Math.floor(Math.random()*3);
-// return random_string[randomchoices];
-console.log(random_string[randomchoices]);
+return random_string[randomchoices];
+// console.log(random_string[randomchoices]);
 }
 
-getComputerGeneretion();
+// getComputerGeneretion();
+
+function getUserChoices(){
+    let userInput=prompt("enter your choices (String rock/paper/scissor)");
+    while(!["rock","paper","scissor"].includes(userInput)){
+        alert("Invalid input ,Please enter String rock/paper/scissor")
+        userInput=prompt("Enter your choices (String rock/paper/scissor)");
+    }
+    console.log(userInput);
+    // return userInput;
+}
+getUserChoices();
